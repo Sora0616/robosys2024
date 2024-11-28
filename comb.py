1f7714bd6869e13b83bbd585bb18dfd4875962a8
Sora0616 <s23c1115wh@s.chibakoudai.jp>
2024-11-28 13:22:28 +0900

    Add a command

---
+++ comb.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024 Sora Hirano
+# SPDX-License-Identifier: BSD-3-Clause
+
+import sys
+import math
+
+def combinations(n, r):
+    return math.factorial(n) // (math.factorial(r) * math.factorial(n - r))
+
+def main():
+    for line in sys.stdin:
+        user_input = line.strip()
+        if not user_input:
+            print("無効な入力: ")
+            sys.exit(1)
+        try:
+            n, r = map(int, user_input.split())
+            result = combinations(n, r)
+            print(f"{n}C{r} = {result}")
+        except ValueError:
+            print(f"無効な入力: {user_input}")
+            sys.exit(1)  
+
+if __name__ == "__main__":
+    main()
+
