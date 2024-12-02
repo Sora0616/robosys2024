92cb7d0c473537b71b897b412e5681521df1e4bb
Sora0616 <s23c1115wh@s.chibakoudai.jp>
2024-12-02 22:13:53 +0900

    combination.py更新

--- combination.py
+++ combination.py
@@ -11,12 +11,12 @@ def combination(n, r):
 for line in sys.stdin:
     user_input = line.strip()
     if not user_input:
-        print("無効な入力: ")
+        print("エラー")
         sys.exit(1)
     try:
         n, r = map(int, user_input.split())
         result = combination(n, r)
         print(f"{n}C{r} = {result}")
     except ValueError:
-        print(f"無効な入力: {user_input}")
+        print(f"エラー")
         sys.exit(1)  
